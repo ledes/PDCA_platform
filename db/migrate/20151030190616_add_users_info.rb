3855c015f5ef6482620c4ae6759da34990511290
Pablo <pablo.ledesma.p@gmail.com>
2015-10-30 15:31:36 -0400

    Add user info to table

---
+++ db/migrate/20151030190616_add_users_info.rb
@@ -0,0 +1,8 @@
+class AddUsersInfo < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+    end
+  end
+end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,9 +2,10 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
+    first_name "John"
+    last_name "Smith"
     sequence(:email) {|n| "user#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
   end
-
 end
