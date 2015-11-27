090ee9d2851b0917db206aa7fcbf34b887fe2b67
Pablo <pablo.ledesma.p@gmail.com>
2015-11-27 10:19:48 -0500

    Test for quizz model

---
+++ spec/models/quizz_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe Quizz, type: :model do
+  it { should belong_to(:course) }
+  it { should belong_to(:user) }
+
+
+  it { should validate_presence_of(:question) }
+  it { should validate_presence_of(:right_answer) }
+  it { should validate_presence_of(:response_A) }
+end
