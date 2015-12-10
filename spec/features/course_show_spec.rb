b472aa069f420e53d89478db70e35b06e5fcf839
Pablo <pablo.ledesma.p@gmail.com>
2015-12-10 16:30:38 -0500

    More test

--- spec/features/course_show_spec.rb
+++ spec/features/course_show_spec.rb
@@ -16,4 +16,28 @@ feature "user sees a courses", %{
     expect(page).to have_content(@course.title)
     expect(page).to have_content(@lesson.title)
   end
+
+  scenario "user can join a course" do
+    visit course_path(@course)
+    click_on "Join course"
+    expect(page).to have_content("You can start the course now")
+    expect(page).to_not have_content("Join course")
+  end
+
+  scenario "user sees list of lessons as links after joining a course" do
+    lesson = FactoryGirl.create(:lesson, course: @course)
+    visit course_path(@course)
+    expect(page).to_not have_selector(".links")
+    click_on "Join course"
+    expect(page).to have_selector(".links")
+  end
+
+  scenario "user sees link to take the quizz" do
+
+  end
+
+  scenario "user clicks on quiz link" do
+
+  end
+
 end
