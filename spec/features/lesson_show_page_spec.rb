86423ce46742ac031c3d7cfe83028bea8ff09253
Pablo <pablo.ledesma.p@gmail.com>
2016-01-01 13:23:34 -0500

    implement lesson show page

--- spec/features/lesson_show_page_spec.rb
+++ spec/features/lesson_show_page_spec.rb
@@ -12,8 +12,29 @@ feature "lesson show page for a user", %{
     login(user)
   end
 
+
+  scenario "user has to joing the course to be able to see the lessons"
+
+  end
+
   scenario "user visits lesson show page" do
     visit course_lesson_path(@course, @lesson)
     expect(page).to have_content(@lesson.title)
+  end
+  
+  scenario "User sees all the lessons for the course" do
+
+  end
+
+
+  scenario "Users sees all the chapters for a lesson" do
+
+  end
+
+  scenario "User sees the right buttons for the quiz or next/previous lesson" do
+
+  end
+
+  scenario "User clicks on the quiz or next/previous lesson button" do
   end
 end
